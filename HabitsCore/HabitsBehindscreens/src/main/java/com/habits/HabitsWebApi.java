2d0968d2a9fe81e958797b2de5544bae65615a27
Revy <revy@github.com>
2018-09-18 00:22:10 +0530

    HabitsCore
    Committed spring setup

---
+++ HabitsCore/HabitsBehindscreens/src/main/java/com/habits/HabitsWebApi.java
@@ -0,0 +1,15 @@
+package com.habits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitsWebApi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(HabitsWebApi.class, args);
+
+	}
+
+}
