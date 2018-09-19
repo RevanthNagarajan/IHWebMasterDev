8406147587c52d133e55cb8d45fe9f7666bc9a07
Revy <revanthnagarajan536@gmail.com>
2018-09-20 03:35:57 +0530

    IHWeb
    Included style variables, font awsome icons, fonts, login page

--- IHWeb/src/app/app-routing/app-routing.module.ts
+++ IHWeb/src/app/app-routing/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { FoundationComponent } from '../foundation/foundation.component'
+import { FoundationComponent } from '../foundation/foundation.component';
+import { LoginComponent } from '../login/login.component';
 
 const routes : Routes = [
 	{
 		path: '',
 		component: FoundationComponent,
+	},
+	{
+		path: 'login',
+		component: LoginComponent,
 	}
 ];
 @NgModule({
--- IHWeb/src/app/app.module.ts
+++ IHWeb/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { FoundationComponent } from './foundation/foundation.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		FoundationComponent
+		FoundationComponent,
+		LoginComponent
 	],
 	imports: [
 		BrowserModule,
+		AngularFontAwesomeModule,
 		// FormsModule,
 		// HttpModule,
 		AppRoutingModule,
